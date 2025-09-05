81252f7d28becd4ecf7be1e4d6c145bdee9f40df
mrubin01 <mi.rubino@protonmail.com>
2025-09-05 12:20:13 +0100

    Added return stat for empty strings

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,7 @@
 const repeatString = function (word, times) {
+  if (!word) return ""
   if (times < 0 || !word) return "ERROR";
+  
   let string = "";
   for (let i = 0; i < times; i++) {
     string += word;
