3df5e53985b778f206ba4aaa06e8ff4a4b3fa07c
mrubin01 <mi.rubino@protonmail.com>
2025-09-07 18:18:20 +0100

    Fixed issue with parameter

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -2,8 +2,8 @@ const convertToCelsius = function(temperature) {
   return Math.round((temperature - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
-  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+const convertToFahrenheit = function(temperature) {
+  return Math.round(((temperature * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
