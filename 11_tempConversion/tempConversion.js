498f08a0916e1fc30f550d920ba4ff5c444f55d5
mrubin01 <mi.rubino@protonmail.com>
2025-09-07 18:16:04 +0100

    Implemented second function

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -3,6 +3,7 @@ const convertToCelsius = function(temperature) {
 };
 
 const convertToFahrenheit = function() {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
