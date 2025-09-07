c6006fa48cc23e6863bc77fe0df3b1056a1e02f2
mrubin01 <mi.rubino@protonmail.com>
2025-09-07 10:17:44 +0100

    Implemented function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function(arr, item) {
-    if (!arr || !item) return "";
+const removeFromArray = function(arr, ...args) {
+    if (!arr) return "";
+
+    item_arrays = []
+    args.forEach(item => {
+        if (!item_arrays.includes(item)) {
+            item_arrays.push(item); 
+        }
+    });
+
+    return arr.filter(item => !item_arrays.includes(item))
+
 };
 
 // Do not edit below this line
