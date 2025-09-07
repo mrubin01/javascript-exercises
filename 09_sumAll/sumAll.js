ee02f60ff7ef8c861778b1a3ebfe17004b16f749
mrubin01 <mi.rubino@protonmail.com>
2025-09-07 11:08:42 +0100

    Added code to establish min and max

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -2,6 +2,10 @@ const sumAll = function(int1, int2) {
     if (!Number.isInteger(int1) || !Number.isInteger(int2) 
     || int1 < 0 || int2 < 0 || int1 === int2) return "ERROR"; 
 
+    // evaluate which parameter is smaller
+    const start = Math.min(int1, int2);
+    const end = Math.max(int1, int2); 
+
     let sum = 0;
     for (let i = int1; i <= int2; i++) {
         sum += i; 
