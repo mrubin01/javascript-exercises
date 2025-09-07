83e4db2b702cf2d10ee333d2dedee892428ffaf2
mrubin01 <mi.rubino@protonmail.com>
2025-09-07 17:31:31 +0100

    Fixed logic

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -3,6 +3,8 @@ const leapYears = function(year) {
 
     if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
         return true; 
+    } else if (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) {
+        return true; 
     } else if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) {
         return false; 
     } else {return false}; 
