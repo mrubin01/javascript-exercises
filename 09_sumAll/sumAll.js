f338712a5e1d1139f330be627befd4e24b776e53
mrubin01 <mi.rubino@protonmail.com>
2025-09-07 11:02:15 +0100

    Modified return stat

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(int1, int2) {
     if (!Number.isInteger(int1) || !Number.isInteger(int2) 
-    || int1 < 0 || int2 < 0 || int1 === int2) return console.log("ERROR");
+    || int1 < 0 || int2 < 0 || int1 === int2) return "ERROR"; 
 
     let sum = 0;
     for (let i = int1; i <= int2; i++) {
