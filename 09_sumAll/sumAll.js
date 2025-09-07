1ce11e97c521a43c2bc71b325c734404d3405bdd
mrubin01 <mi.rubino@protonmail.com>
2025-09-07 10:50:40 +0100

    Validate that arguments are positive integers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,10 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (!Number.isInteger(int1) || !Number.isInteger(int2) 
+    || int1 < 0 || int2 < 0 || int1 === int2) return console.log("ERROR");
+
+    
+
+
 
 };
 
