58155c46e7320ab74b491598e022e6ffc412d3c0
mrubin01 <mi.rubino@protonmail.com>
2025-09-07 11:00:56 +0100

    Implemented function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -2,8 +2,11 @@ const sumAll = function(int1, int2) {
     if (!Number.isInteger(int1) || !Number.isInteger(int2) 
     || int1 < 0 || int2 < 0 || int1 === int2) return console.log("ERROR");
 
-    
-
+    let sum = 0;
+    for (let i = int1; i <= int2; i++) {
+        sum += 1; 
+    }
+    return sum; 
 
 
 };
