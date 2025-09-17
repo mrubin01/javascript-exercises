7923bc528c77a5fa03f3ffc5546ab12020ce1a50
mrubin01 <mi.rubino@protonmail.com>
2025-09-17 11:50:13 +0100

    Add functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2; 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2; 	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  s = 0;
+
+  for (let item of arr) {
+    if (typeof item === "number" && !isNaN(item)) {
+      s += item;
+    }
+  }
+
+  return s;	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  t = 1;
+
+  for (let item of arr) {
+    if (typeof item === "number" && !isNaN(item)) {
+      t *= item;
+    }
+  }
+
+  return t; 
 
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let tot = 1;
+	if (num === 0 || num === 1) {
+    return tot;
+  } else if (num > 1) {
+    for (i = num; i > 1; i--) {
+      i * tot;
+    }
+    return tot;
+  }
 };
 
 // Do not edit below this line
