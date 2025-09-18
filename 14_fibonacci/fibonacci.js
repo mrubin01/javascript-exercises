bb004d4333deac3c47ae16eb69c0ab231bc67de9
mrubin01 <mi.rubino@protonmail.com>
2025-09-18 11:20:27 +0100

    Implement function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num !== "number" || Number.isInteger(num)) {
+        return 0;
+    }
+
+    if (num === 1 || num === 2) {
+        return 1;
+    }
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= nnum; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
 
 };
 
