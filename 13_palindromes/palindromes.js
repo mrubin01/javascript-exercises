828b0a7176c575650f816c19221a57e838dec48d
mrubin01 <mi.rubino@protonmail.com>
2025-09-18 10:50:41 +0100

    Fix if-else stat

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,9 +1,9 @@
 const palindromes = function (raw_str) {
-    if (typeof raw_str === "string" && raw_str.length > 0) {
-        let str = raw_str.toLowerCase().replace(/[^a-z0-9]/g, ""); 
-    } else {
+    if (typeof raw_str !== "string" || raw_str.length === 0) {
         return false; 
-    }
+    } 
+
+    let str = raw_str.toLowerCase().replace(/[^a-z0-9]/g, ""); 
 
     let rev_str = str.split("").reverse().join(""); 
     return str === rev_str;
