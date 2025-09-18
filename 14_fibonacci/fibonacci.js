c8d511ea367dd4312db601257b89f83716849714
mrubin01 <mi.rubino@protonmail.com>
2025-09-18 12:40:25 +0100

    Refactor function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,23 +1,21 @@
 function fibonacci(n) {
-  // convert strings numbers
-  n = Number(n);
+    // convert strings to numbers
+    n = Number(n);
+    // validate input
+    if (!Number.isInteger(n) || n < 0) {
+        return "OOPS";
+    }
 
-  // validate input
-  if (!Number.isInteger(n) || n < 1) {
-    console.log("OOPS");
-    return;
-  }
+    if (n === 0) return 0;
+    if (n === 1) return 1;
 
-  if (n === 0) return 0;
-  if (n === 1 || n === 2) return 1;
-
-  let a = 1, b = 1;
-  for (let i = 3; i <= n; i++) {
-    const next = a + b;
-    a = b;
-    b = next;
-  }
-  return b;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        const next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 }
 
 
