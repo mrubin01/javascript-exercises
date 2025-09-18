25ee59fcb582d67bbac9bc4fb15e3a2e4c8635c8
mrubin01 <mi.rubino@protonmail.com>
2025-09-18 14:27:40 +0100

    Implement function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    if (!Array.isArray(arr) || arr.length === 0) {
+        return null;
+    }
+
+    let oldest = arr[0];
+
+    let maxAge = oldest.death - oldest.birth;
+
+    for (let i = 1; i < people.length; i++) {
+        let age = arr[i].death - arr[i].birth;
+
+        if (age > maxAge) {
+            maxAge = age;
+            oldest = arr[i]; 
+        }
+    }
+
+    return oldest.name;
 
 };
 
